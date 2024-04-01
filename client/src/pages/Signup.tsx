120b4dbdca74698ee7a1a937c5e22df2b6fe8db0
Dmitrijus <[boyuda96@gmail.com]>
2024-04-01 18:10:47 +0300

    Comment to identify error messages

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -41,6 +41,7 @@ export default function Signup() {
         body: JSON.stringify(formData),
       });
       const data = await res.json();
+      // TODO: add more messages
       if (data.success === false) {
         setLoading(false);
         return setErrorMessage(data.message);
