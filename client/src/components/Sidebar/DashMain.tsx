1591f9b768e201b55c0e2afe79aa953da6657d17
Dmitrijus <[boyuda96@gmail.com]>
2024-04-11 17:16:07 +0300

    expenses are shown on main tab. added array for future pie chart

--- client/src/components/Sidebar/DashMain.tsx
+++ client/src/components/Sidebar/DashMain.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
-import { Button } from 'flowbite-react';
+
 import {
   HiCurrencyEuro,
   HiArrowNarrowUp,
@@ -9,19 +9,51 @@ import {
   HiOutlineInformationCircle,
 } from 'react-icons/hi';
 
+// Interfaces
+interface UserIncome {
+  _id: string;
+  userId: string;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
+
+interface IncomeCategory {
+  category: string;
+  amount: number;
+}
+
 export default function DashMain() {
   ////////
   // States
   ///////
+  // Incomes
   const [userIncomes, setUserIncomes] = useState([]);
   const [userIncomesSum, setUserIncomesSum] = useState(0);
   const [userIncomeCount, setUserIncomesCount] = useState(0);
+  const [userIncomesByCategory, setUserIncomesByCategory] = useState<
+    IncomeCategory[]
+  >([]);
+  // Expenses
+  const [userExpenses, setUserExpenses] = useState([]);
+  const [userExpensesSum, setUserExpensesSum] = useState(0);
+  const [userExpensesCount, setUserExpensesCount] = useState(0);
+  // Current user
   const { currentUser } = useSelector((state: RootState) => state.user);
   // Expenses state
 
   useEffect(() => {
     fetchIncomes();
+    fetchExpenses();
+    setIncomeCategory();
+    console.log(userExpensesCount, userExpensesSum, userExpenses);
     console.log(userIncomeCount, userIncomesSum, userIncomes);
+    console.log(userIncomesByCategory);
   }, [currentUser]);
 
   ////////
@@ -39,6 +71,36 @@ export default function DashMain() {
     } catch (error: any) {
       console.log(error.message);
     }
+  };
+
+  const fetchExpenses = async () => {
+    try {
+      const res = await fetch('/api/transactions/get-expense?limit=1000');
+      const data = await res.json();
+      if (res.ok) {
+        setUserExpenses(data.expenses);
+        setUserExpensesCount(data.totalExpenseCount);
+        setUserExpensesSum(data.total);
+      }
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+
+  const setIncomeCategory = function () {
+    const incomeArray: [string, number][] = [];
+    userIncomes.forEach((item: UserIncome) => {
+      incomeArray.push([item.category, item.amount]);
+    });
+
+    const incomeCategoryArray: IncomeCategory[] = incomeArray.map(
+      ([category, amount]) => ({
+        category,
+        amount,
+      })
+    );
+
+    setUserIncomesByCategory(incomeCategoryArray);
   };
 
   return (
@@ -66,14 +128,14 @@ export default function DashMain() {
               <h3 className="text-gray-500 text-md uppercase">
                 Total Expenses
               </h3>
-              <p className="text-2xl">€{userIncomesSum}</p>
+              <p className="text-2xl">€{userExpensesSum}</p>
             </div>
             <HiArrowNarrowDown className="text-red-600 text-5xl shadow-lg p-3 rounded-full" />
           </div>
           <div className="flex gap-2 text-sm">
             <span className=" flex items-center">
               <HiOutlineInformationCircle />
-              {userIncomeCount}
+              {userExpensesCount}
             </span>
             <div className="text-gray-500">Expenses Records</div>
           </div>
@@ -84,7 +146,7 @@ export default function DashMain() {
               <h3 className="text-gray-500 text-md uppercase">
                 Current Balance
               </h3>
-              <p className="text-2xl">€{userIncomesSum}</p>
+              <p className="text-2xl">€{userIncomesSum - userExpensesSum}</p>
             </div>
             <HiCurrencyEuro className="text-black-600 text-5xl shadow-lg p-3 rounded-full" />
           </div>
